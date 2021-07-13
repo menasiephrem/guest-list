b530e928ed589efd562588b2c14b8112a98f583d
Menasi Ephrem <menasiephrem@gmail.com>
2021-07-13 18:13:06 +0300

    :zap: improve perfromance

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const {format} = require('util');
+require('events').EventEmitter.defaultMaxListeners = 0
 const path = require('path')
 const utils = require('util')
 const puppeteer = require('puppeteer')
@@ -13,9 +13,14 @@ app.use(bodyParser.json());
 
 app.post('/downloadReport', async (req, res) =>{
     const {body} = req;
+    console.log("open Browser");
+    const browser = await openCloseBrowser()
     for(let i = 0; i < body.length; i++){
-        writeFile(body[i])
+        writeFile(body[i], browser)
     }
+    console.log("Close browser");
+   // openCloseBrowser(browser)
+
     res.json({done: "ok"})
 });
 
@@ -32,15 +37,15 @@ async function getTemplateHtml(pathToHtml) {
     }
 }
 
-async function writeFile(data)  {
+async function writeFile(data, browser)  {
     console.log(data);
     const {NAME: name, isMiya} = data;
     const path = isMiya?.toLowerCase() === "true" ? "./html/invitation_miya.html" : "./html/invitation_menasi.html"
     data.NAME = `${data.title} ${name}`
     data.RSVP = data.RSVP ?? ""
-    const ret = await generatePdf(data, path)
-    let newFileName = `${isMiya?.toLowerCase() === "true" ? "miya_": "dumbo_"}${name}_${Date.now()}.pdf`;
 
+    const ret = await generatePdf(data, path, browser)
+    let newFileName = `${isMiya?.toLowerCase() === "true" ? "miya_": "dumbo_"}${name}_${Date.now()}.pdf`;
     fs.writeFile(newFileName, ret,{ flag: 'wx' }, (err) => {
         if(err) {
             return console.log(err);
@@ -51,23 +56,33 @@ async function writeFile(data)  {
 }
 
 
-async function generatePdf(data, path) {
-
+async function generatePdf(data, path, browser) {
     const res = await getTemplateHtml(path)
 
     const template = hb.compile(res, { strict: true })
     const result = template(data)
     const html = result;
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-        ]
-      });
+
     const page = await browser.newPage()
+    await page.setDefaultNavigationTimeout(0); 
     await page.setContent(html);
     const Buffer = await page.pdf({  format: 'A4' })
-    await browser.close();
+    await page.close()
     return Buffer;  
+}
+
+const openCloseBrowser = async (browser) => {
+    if(!browser){
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: [
+              '--no-sandbox',
+              '--disable-setuid-sandbox',
+            ]
+          });
+          return browser;
+    }else{
+        await browser.close();
+    }
+
 }
